20eb2bd8a1b3cb1ce933f9739257c9b5e47e5ffc
Tim Morgan <tim@timmorgan.org>
2024-06-14 08:38:50 -0500

    Give better feedback when params are missing or wrong

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require 'sinatra'
 STATS_PATH = File.expand_path('./storage/stats.json', __dir__)
 SELF_HOSTED_STATS_PATH = File.expand_path('./storage/self_hosted_stats.json', __dir__)
 CURRENT_STATS_PATH = File.expand_path('./storage/current.json', __dir__)
-SECRET = ENV['SECRET'].freeze
+SECRET = ENV.fetch('SECRET').freeze
 
 get '/' do
   redirect 'https://natalie-lang.org'
@@ -17,15 +17,16 @@ get '/stats' do
 end
 
 post '/stats' do
-  if params[:secret].to_s != SECRET
-    halt 401, 'must pass "secret" param with correct secret'
-  end
-  unless params[:stats]
-    halt 400, 'must pass "stats" param with json string'
-  end
+  halt 401, 'missing "secret" param' unless params[:secret]
+  halt 401, "\"secret\" param (size #{params[:secret].size}) does not match" if params[:secret] != SECRET
+  halt 400, 'missing "stats" param' unless params[:stats]
   stats = JSON.parse(File.read(STATS_PATH)) rescue []
 
-  new_stats = JSON.parse(params[:stats])
+  begin
+    new_stats = JSON.parse(params[:stats])
+  rescue JSON::ParserError
+    halt 400, '"stats" param must be valid JSON'
+  end
   current_time = Time.now.utc.strftime('%Y-%m-%dT%H:%M:%SZ')
 
   details = new_stats.delete("Details")
